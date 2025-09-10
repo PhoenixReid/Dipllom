84242bc8e6f89985278f280af596951630191530
Phoenix <phoenixreid@mail.ru>
2025-09-10 16:29:46 +0300

    Добавление новых тестов

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/addEditNewsTest.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/addEditNewsTest.java
@@ -23,6 +23,7 @@ import static org.hamcrest.Matchers.is;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.runner.AndroidJUnit4;
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
 
@@ -30,6 +31,7 @@ import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
 import java.text.SimpleDateFormat;
@@ -39,6 +41,7 @@ import java.util.concurrent.TimeUnit;
 
 import ru.iteco.fmhandroid.R;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(AndroidJUnit4.class)
 public class addEditNewsTest {
 
     @Rule
@@ -135,7 +138,7 @@ public class addEditNewsTest {
 
 
     @Test
-    public void test2_EditNewsTest() {
+    public void test2_EditHeadingNewsTest() {
 
         onView(withText("Зарплата Аовы")).check(matches(isDisplayed()));
 
@@ -155,13 +158,76 @@ public class addEditNewsTest {
             e.printStackTrace();
         }
 
-        onView(withText("Объявление"))
-                .inRoot(isPlatformPopup())  // важно: ищем во всплывающем окне
-                .check(matches(isDisplayed()))
-                .perform(click());
-
         onView(withId(R.id.news_item_title_text_input_edit_text))
                 .perform(replaceText("Праздник в связи с зарплатой Аовы"));
+
+        onView(withId(R.id.save_button))
+                .perform(click());
+
+        onView(withText("Праздник в связи с зарплатой Аовы")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test3_EditDescriptioningNewsTest() {
+
+        onView(withText("Праздник в связи с зарплатой Аовы")).check(matches(isDisplayed()));
+
+        onView(withId(R.id.edit_news_material_button))
+                .perform(click());
+
+        onView(allOf(withId(R.id.edit_news_item_image_view),
+                withParent(hasDescendant(withText("Праздник в связи с зарплатой Аовы")))))
+                .perform(click());
+
+        onView(withId(R.id.news_item_category_text_input_layout))
+                .perform(click());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+        // 9. Ввести описание
+        onView(withId(R.id.news_item_description_text_input_edit_text))
+                .perform(replaceText("Праздник в честь того, что Аовина зарплата превысила вашу"), closeSoftKeyboard());
+
+        onView(withId(R.id.save_button))
+                .perform(click());
+
+        onView(withText("Праздник в связи с зарплатой Аовы")).check(matches(isDisplayed()));
+        onView(withText("Праздник в связи с зарплатой Аовы")).perform(click());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText("Праздник в честь того, что Аовина зарплата превысила вашу")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test4_EditDataNewsTest() {
+
+        onView(withText("Праздник в связи с зарплатой Аовы")).check(matches(isDisplayed()));
+
+        onView(withId(R.id.edit_news_material_button))
+                .perform(click());
+
+        onView(allOf(withId(R.id.edit_news_item_image_view),
+                withParent(hasDescendant(withText("Праздник в связи с зарплатой Аовы")))))
+                .perform(click());
+
+        onView(withId(R.id.news_item_category_text_input_layout))
+                .perform(click());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // 7. Установить дату публикации
         onView(withId(R.id.news_item_publish_date_text_input_edit_text))
@@ -182,9 +248,6 @@ public class addEditNewsTest {
                 .perform(click());
 
 
-        // 9. Ввести описание
-        onView(withId(R.id.news_item_description_text_input_edit_text))
-                .perform(replaceText("Праздник в честь того, что Аовина зарплата превысила вашу"), closeSoftKeyboard());
 
         onView(withId(R.id.save_button))
                 .perform(click());
@@ -197,13 +260,98 @@ public class addEditNewsTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        onView(withText("Праздник в честь того, что Аовина зарплата превысила вашу")).check(matches(isDisplayed()));
         onView(withText(tomorrowDate)).check(matches(isDisplayed()));
     }
+    @Test
+    public void test5_EditCategoryNewsTest() {
 
+        onView(withText("Праздник в связи с зарплатой Аовы")).check(matches(isDisplayed()));
+
+        onView(withId(R.id.edit_news_material_button))
+                .perform(click());
+
+        onView(allOf(withId(R.id.edit_news_item_image_view),
+                withParent(hasDescendant(withText("Праздник в связи с зарплатой Аовы")))))
+                .perform(click());
+
+        onView(withId(R.id.news_item_category_text_input_layout))
+                .perform(click());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withText("Объявление"))
+                .inRoot(isPlatformPopup())  // важно: ищем во всплывающем окне
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+
+        onView(withId(R.id.save_button))
+                .perform(click());
+
+        onView(withId(R.id.filter_news_material_button))
+                .perform(click());
+
+        onView(withId(R.id.news_item_category_text_auto_complete_text_view))
+                .perform(click());
+
+        onView(withText("Объявление"))
+                .inRoot(isPlatformPopup())
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        onView(withId(R.id.filter_button))
+                .perform(click());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText("Праздник в связи с зарплатой Аовы")).check(matches(isDisplayed()));
+    }
 
     @Test
-    public void test3_DelNwsTest() {
+    public void test6_EditActivionNewsTest() {
+
+        onView(withText("Праздник в связи с зарплатой Аовы")).check(matches(isDisplayed()));
+
+        onView(withId(R.id.edit_news_material_button))
+                .perform(click());
+
+        onView(allOf(withId(R.id.edit_news_item_image_view),
+                withParent(hasDescendant(withText("Праздник в связи с зарплатой Аовы")))))
+                .perform(click());
+
+        onView(withId(R.id.news_item_category_text_input_layout))
+                .perform(click());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.switcher))
+                .perform(click());
+
+        onView(withId(R.id.save_button))
+                .perform(click());
+
+        onView(withText("Праздник в связи с зарплатой Аовы")).check(matches(isDisplayed()));
+
+        onView(withId(R.id.main_menu_image_button))
+                .perform(click());
+        onView(withText("Новости")).perform(click());
+
+        onView(withText("Праздник в связи с зарплатой Аовы")).check(doesNotExist());
+
+    }
+
+    @Test
+    public void test7_DelNwsTest() {
 
         onView(withId(R.id.edit_news_material_button))
                 .perform(click());
